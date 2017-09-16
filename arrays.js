02ac9717dc5f6796d5362823a592d00ec5ba54ae
Sherry <sherryptk@gmail.com>
2017-09-16 20:55:47 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,34 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array,string){
+  var sweets = [...array, string]
+console.log(sweets)
+  return sweets
+}
+
+function destructivelyAddElementToBeginningOfArray(array,string){
+return array.unshift(string)
+}
+
+destructivelyAddElementToBeginningOfArray(chocolateBars,'crunch')
+
+addElementToBeginningOfArray(chocolateBars, 'milky way')
+
+function destructivelyAddElementToEndOfArray(array,string){
+return array.push(string)
+}
+
+destructivelyAddElementToEndOfArray(chocolateBars,'crunch')
+
+function addElementToEndOfArray(array,string){
+  var sweets = [...array, string]
+  console.log(sweets)
+  return sweets
+}
+
+addElementToEndOfArray(chocolateBars, 'twizzler')
+
+
+
+// TODO: Define a function in arrays.js called accessElementInArray. 
+// The function should accept an array and an index and return the element at that index.
