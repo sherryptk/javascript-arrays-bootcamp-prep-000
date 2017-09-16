61935ed0ba94c12a8f12b4a40d91fac8c8b6c571
Sherry <sherryptk@gmail.com>
2017-09-16 21:33:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,4 +25,7 @@ function accessElementInArray(array,index){
   return array[index]
 }
 
-// The function should accept an array and an index and return the element at that index.
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
