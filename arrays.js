24ddb3efb79beb200d3b0e1e03513e4f89817c40
Sherry <sherryptk@gmail.com>
2017-09-16 21:10:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array,element){
-  var sweets
-  sweets=[element,...array]
+  var sweets=[element,...array]
   return sweets
 }
 
